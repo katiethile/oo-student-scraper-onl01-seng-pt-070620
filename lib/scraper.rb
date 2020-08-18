fc726bd1f772adfa9eb1ca6a8c7e4ca550deafc4
Katie Le <lethikatie@gmail.com>
2020-08-18 21:01:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -38,7 +38,7 @@ class Scraper
               student[:blog] = link 
             end 
           end
-          student[:profile_quote]=doc.css(".profile-quote")
+          student[:profile_quote]=doc.css(".profile-quote").text
           student
         end 
    end 
