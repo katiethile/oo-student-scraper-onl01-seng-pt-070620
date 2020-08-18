666370569a95a038b329a831a1641e7766c07e40
Katie Le <lethikatie@gmail.com>
2020-08-18 20:09:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,40 @@ require 'pry'
 
 class Scraper
 
+  @@student = []
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
+   times_to_loop = doc.css(".student-card").size
+    scraped_students = []
     
-  end
+    i = 0 
+    doc.css(".roster-cards-container").each do |student|
+    times_to_loop.times do 
+      @@student.push({
+        location: doc.css(".student-location")[i].text, 
+        name: doc.css(".student-name")[i].text,
+        profile_url: doc.css(".student-card a")[i].attr("href")
+      })
+      i += 1 
+      end 
+    end 
+    @@student
+  end 
 
   def self.scrape_profile_page(profile_url)
-    
-  end
-
-end
-
+    students = {}
+    doc = Nokogiri::HTML(open(profile_url))
+    all_students = doc.css(".social-icon-container").children.css("a").map {|el| el.attribute("href").value}
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link 
+        elsif link.include?("github")
+          student[:github] = link 
+          elsif link.include?("twitter")
+            student[:twitter] = link 
+            else link.include?("blog")
+              student[:blog] = link 
+            end 
+          end 
+        end 
+   end 
