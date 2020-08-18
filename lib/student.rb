e86218d5814e367c40a9085fca1f064841b0072f
Katie Le <lethikatie@gmail.com>
2020-08-18 21:16:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,10 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_hash[:name] 
+    student_hash.each do |key, value|
+      self.
+      
+    end 
     @@all << self 
   end
 
