2aa53d3c318694c8927a65eb4df52008517ca488
Katie Le <lethikatie@gmail.com>
2020-08-18 20:27:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,7 +34,7 @@ class Scraper
           student[:github] = link 
           elsif link.include?('twitter')
             student[:twitter] = link 
-            else link.include?('blog')
+            else 
               student[:blog] = link 
             end 
           end 
